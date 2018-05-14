94f43ac8c78f2440545bbd55084f8a4912b1e1fc
linlexing <linlexing@gmail.com>
2018-05-14 18:06:03 +0800

    1

--- login.ts
+++ login.ts
@@ -6,7 +6,7 @@ import { IElement } from './model/element';
 export class Login {
     public static login(userName: string, pwd: string, cb: (ok: boolean, err: string) => void): void {
         axios.get("/login?_api=login").then((value) => {
-
+            return ;
         }).catch( (error) =>{
             // tslint:disable-next-line:no-console
             console.log(error);
