cf427fa79e9cf0ed694bf43275e76e97b209d40b
linlexing <linlexing@gmail.com>
2018-06-02 22:56:28 +0800

    去除多余的测试模块

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import Index from "./index/index";
-import recordview from "./recordview";
 export { Index };
 export default {
-    index: Index,
-    recordview
+    index: Index
 };
--- recordview/action.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from "typesafe-actions";
-
-const changeUserName = createAction("[recordview]changeUserName", resolve => (val: string) => resolve(val));
-export { changeUserName };
